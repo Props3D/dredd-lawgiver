d949b6c377484c7a740a3f6a9aab51c4a20a76ce
Props3D <88125656+Props3D@users.noreply.github.com>
2023-01-06 16:59:36 -0800

    Update easyvr.h

--- dredd-lawgiver/easyvr.h
+++ dredd-lawgiver/easyvr.h
@@ -6,109 +6,38 @@
 #include "SoftwareSerial.h"
 #include <avr/pgmspace.h>
 
-#define VR_DEFAULT_TIMEOUT (1000)
-
-/***************************************************************************/
+/**
+ * Defines for packet frame
+ */
 #define FRAME_HEAD (0xAA)
 #define FRAME_END (0x0A)
-
-/***************************************************************************/
-#define FRAME_CMD_CHECK_SYSTEM (0x00)
-#define FRAME_CMD_CHECK_BSR (0x01)
-#define FRAME_CMD_CHECK_TRAIN (0x02)
-#define FRAME_CMD_CHECK_SIG (0x03)
-
-#define FRAME_CMD_RESET_DEFAULT (0x10)  //reset configuration
-#define FRAME_CMD_SET_BR (0x11)         //baud rate
-#define FRAME_CMD_SET_IOM (0x12)        //IO mode
-#define FRAME_CMD_SET_PW (0x13)         //pulse width
-#define FRAME_CMD_RESET_IO (0x14)       // reset IO OUTPUT
-#define FRAME_CMD_SET_AL (0x15)         // Auto load
-
-#define FRAME_CMD_TRAIN (0x20)
-#define FRAME_CMD_SIG_TRAIN (0x21)
-#define FRAME_CMD_SET_SIG (0x22)
-
-#define FRAME_CMD_LOAD (0x30)         //Load N records
-#define FRAME_CMD_CLEAR (0x31)        //Clear BSR buffer
-#define FRAME_CMD_GROUP (0x32)        //
-#define FRAME_CMD_GROUP_SET (0x00)    //
-#define FRAME_CMD_GROUP_SUGRP (0x01)  //
-#define FRAME_CMD_GROUP_LSGRP (0x02)  //
-#define FRAME_CMD_GROUP_LUGRP (0x03)  //
-#define FRAME_CMD_GROUP_CUGRP (0x04)  //
-
-#define FRAME_CMD_TEST (0xEE)
-#define FRAME_CMD_TEST_READ (0x01)
-#define FRAME_CMD_TEST_WRITE (0x00)
-
-
 #define FRAME_CMD_VR (0x0D)  //Voice recognized
-#define FRAME_CMD_PROMPT (0x0A)
 #define FRAME_CMD_ERROR (0xFF)
 
+#define VR_DEFAULT_TIMEOUT (1000)
 
 class EasyVR : public SoftwareSerial {
 public:
-  typedef enum {
-    PULSE = 0,
-    TOGGLE = 1,
-    SET = 2,
-    CLEAR = 3
-  } io_mode_t;
-
-  typedef enum {
-    LEVEL0 = 0,
-    LEVEL1,
-    LEVEL2,
-    LEVEL3,
-    LEVEL4,
-    LEVEL5,
-    LEVEL6,
-    LEVEL7,
-    LEVEL8,
-    LEVEL9,
-    LEVEL10,
-    LEVEL11,
-    LEVEL12,
-    LEVEL13,
-    LEVEL14,
-    LEVEL15,
-  } pulse_width_level_t;
-
-  typedef enum {
-    GROUP0 = 0,
-    GROUP1,
-    GROUP2,
-    GROUP3,
-    GROUP4,
-    GROUP5,
-    GROUP6,
-    GROUP7,
-    GROUP_ALL = 0xFF,
-  } group_t;
-
-
   /**
-	 * VR class constructor.
-	 *   receivePin --> software serial RX
-	 *   transmitPin --> software serial TX
+  * VR class constructor.
+  *   receivePin --> software serial RX
+  *   transmitPin --> software serial TX
    */
   EasyVR(uint8_t receivePin, uint8_t transmitPin)
     : SoftwareSerial(receivePin, transmitPin) {
   }
 
   /**
-	 * Voice recognition routine. Detects when trained command is recognized.
-	 *   buf --> return data
-	 *	 buf[0]  -->  Group mode(FF: None Group, 0x8n: User, 0x0n:System
-   *          buf[1]  -->  number of record which is recognized. 
-   *          buf[2]  -->  Recognizer index(position) value of the recognized record.
-   *          buf[3]  -->  Signature length
-   *          buf[4]~buf[n] --> Signature
-	 *	 timeout --> wait time for receiving packet.
-	 * Returns length of valid data in buf
-   *   0 means no data received.
+   * Voice recognition routine. Detects when trained command is recognized.
+   *  buf --> return data
+   *    buf[0]  -->  Group mode(FF: None Group, 0x8n: User, 0x0n:System
+   *    buf[1]  -->  number of record which is recognized. 
+   *    buf[2]  -->  Recognizer index(position) value of the recognized record.
+   *    buf[3]  -->  Signature length
+   *    buf[4]~buf[n] --> Signature
+   *    timeout --> wait time for receiving packet.
+   *  Returns length of valid data in buf
+   *    0 means no data received.
    */
   int recognize(uint8_t *buf, int timeout) {
     int ret, i;
